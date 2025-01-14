dd276eee517096214aeb227b5b4ed3b2356d75f7
Danielf003 <robo.daniil@gmail.com>
2025-01-14 03:50:49 +0300

    disable traj acc

--- Traj_thetas.py
+++ Traj_thetas.py
@@ -41,15 +41,15 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         if 0 <= time <= T_b:
             p_s = np.array([time**2, time, 1, 0, 0, 0, 0, 0])
             v_s = np.array([2*time, 1, 0, 0, 0, 0, 0, 0])
-            a_s = np.array([2, 0, 0, 0, 0, 0, 0, 0])
+            # a_s = np.array([2, 0, 0, 0, 0, 0, 0, 0])
         elif T_b < time <= T_f - T_b:
             p_s = np.array([0, 0, 0, time, 1, 0, 0, 0])
             v_s = np.array([0, 0, 0, 1, 0, 0, 0, 0])
-            a_s = np.zeros(8)
+            # a_s = np.zeros(8)
         elif time > T_f - T_b:
             p_s = np.array([0, 0, 0, 0, 0, time**2, time, 1])
             v_s = np.array([0, 0, 0, 0, 0, 2*time, 1, 0])
-            a_s = np.array([0, 0, 0, 0, 0, 2, 0, 0])
+            # a_s = np.array([0, 0, 0, 0, 0, 2, 0, 0])
 
         p_x_s = p_s.dot(C_x)
         p_y_s = p_s.dot(C_y)
@@ -57,10 +57,10 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         v_x_s = v_s.dot(C_x)
         v_y_s = v_s.dot(C_y)
         v_z_s = v_s.dot(C_z)
-        a_x_s = a_s.dot(C_x)
-        a_y_s = a_s.dot(C_y)
-        a_z_s = a_s.dot(C_z)
-        a_z_s = a_s.dot(C_z)
+        # a_x_s = a_s.dot(C_x)
+        # a_y_s = a_s.dot(C_y)
+        # a_z_s = a_s.dot(C_z)
+        # a_z_s = a_s.dot(C_z)
 
         # Решение обратной задачи кинематики
 
@@ -75,14 +75,15 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         else:
             J_inv = J_inv_func(q)
         dq = J_inv.dot(np.array([v_x_s, v_y_s, v_z_s, 0]))
-        dq1, dq2, dq3, dq4 = dq
-        if dJ_dt_func is None:
-            dJ_dt = calc_Jdot(s1,s2,s3,s4,c1,c2,c3,c4,l1,l2,l3,l4,dq1,dq2,dq3,dq4)
-        else:
-            dJ_dt = dJ_dt_func(q, dq)
+        # dq1, dq2, dq3, dq4 = dq
+        # if dJ_dt_func is None:
+        #     dJ_dt = calc_Jdot(s1,s2,s3,s4,c1,c2,c3,c4,l1,l2,l3,l4,dq1,dq2,dq3,dq4)
+        # else:
+        #     dJ_dt = dJ_dt_func(q, dq)
 
         
-        ddq = J_inv.dot(np.array([a_x_s, a_y_s, a_z_s, 0]) - dJ_dt.dot(dq))
+        # ddq = J_inv.dot(np.array([a_x_s, a_y_s, a_z_s, 0]) - dJ_dt.dot(dq))
+        ddq = np.zeros(4)
 
     else:
         # Фаза перемещения thet_B (обратная thet_S) и thet_P
@@ -93,17 +94,17 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         if 0 <= time_2 <= T_b:
             p_b = np.array([time_2**2, time_2, 1, 0, 0, 0, 0, 0])
             v_b = np.array([2*time_2, 1, 0, 0, 0, 0, 0, 0])
-            a_b = np.array([2, 0, 0, 0, 0, 0, 0, 0])
+            # a_b = np.array([2, 0, 0, 0, 0, 0, 0, 0])
 
         elif T_b < time_2 <= T_f - T_b:
             p_b = np.array([0, 0, 0, time_2, 1, 0, 0, 0])
             v_b = np.array([0, 0, 0, 1, 0, 0, 0, 0])
-            a_b = np.zeros(8)
+            # a_b = np.zeros(8)
 
         elif T_f - T_b < time_2 <= T_f + 1:
             p_b = np.array([0, 0, 0, 0, 0, time_2**2, time_2, 1])
             v_b = np.array([0, 0, 0, 0, 0, 2*time_2, 1, 0])
-            a_b = np.array([0, 0, 0, 0, 0, 2, 0, 0])
+            # a_b = np.array([0, 0, 0, 0, 0, 2, 0, 0])
 
         p_x_b = p_b.dot(C_x)
         p_y_b = p_b.dot(C_y)
@@ -111,10 +112,10 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         v_x_b = v_b.dot(C_x)
         v_y_b = v_b.dot(C_y)
         v_z_b = v_b.dot(C_z)
-        a_x_b = a_b.dot(C_x)
-        a_y_b = a_b.dot(C_y)
-        a_z_b = a_b.dot(C_z)
-        a_z_b = a_b.dot(C_z)
+        # a_x_b = a_b.dot(C_x)
+        # a_y_b = a_b.dot(C_y)
+        # a_z_b = a_b.dot(C_z)
+        # a_z_b = a_b.dot(C_z)
 
         # Решение обратной задачи кинематики
 
@@ -132,13 +133,13 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         dq = J_inv.dot(np.array([v_x_b, v_y_b, v_z_b, 0]))
         dq1, dq2, dq3, dq4 = dq
         dq_b = dq
-        if dJ_dt_func is None:
-            dJ_dt = calc_Jdot(s1,s2,s3,s4,c1,c2,c3,c4,l1,l2,l3,l4,dq1,dq2,dq3,dq4)
-        else:
-            dJ_dt = dJ_dt_func(q, dq)
+        # if dJ_dt_func is None:
+        #     dJ_dt = calc_Jdot(s1,s2,s3,s4,c1,c2,c3,c4,l1,l2,l3,l4,dq1,dq2,dq3,dq4)
+        # else:
+        #     dJ_dt = dJ_dt_func(q, dq)
         
-        ddq = J_inv.dot(np.array([a_x_b, a_y_b, a_z_b, 0]) - dJ_dt.dot(dq))
-        ddq_b = ddq
+        # ddq = J_inv.dot(np.array([a_x_b, a_y_b, a_z_b, 0]) - dJ_dt.dot(dq))
+        # ddq_b = ddq
 
         # Расчет theta_P
         qt = [time**4, time**3, time**2, time, 1]
@@ -153,15 +154,16 @@ def thetas_traj(t, T_f, T_b, delta_T, C_x, C_y, C_z, a, J_inv_func=None, dJ_dt_f
         dq3 = np.dot(a[2, :], dqt)
         dq4 = np.dot(a[3, :], dqt)
 
-        ddqt = [12*time**2, 6*time, 2, 0, 0]
-        ddq1 = np.dot(a[0, :], ddqt)
-        ddq2 = np.dot(a[1, :], ddqt)
-        ddq3 = np.dot(a[2, :], ddqt)
-        ddq4 = np.dot(a[3, :], ddqt)
+        # ddqt = [12*time**2, 6*time, 2, 0, 0]
+        # ddq1 = np.dot(a[0, :], ddqt)
+        # ddq2 = np.dot(a[1, :], ddqt)
+        # ddq3 = np.dot(a[2, :], ddqt)
+        # ddq4 = np.dot(a[3, :], ddqt)
         # print (f'q4_s = {q4}')
         # print (f'q_b = {q_b}')
         q = q_b + np.array([q1, q2, q3, q4])
         dq = dq_b + np.array([dq1, dq2, dq3, dq4])
-        ddq = ddq_b + np.array([ddq1, ddq2, ddq3, ddq4])
+        # ddq = ddq_b + np.array([ddq1, ddq2, ddq3, ddq4])
+        ddq = np.zeros(4)
 
     return q, dq, ddq
